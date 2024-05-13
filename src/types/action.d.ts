62eb78ecc683cf57dd75f253582bbf3481f4834e
kdh379 <ehgus37625@naver.com>
2024-05-13 23:41:26 +0900

    chore: add next-bundle-analyzer

--- src/types/action.d.ts
+++ src/types/action.d.ts
@@ -3,18 +3,18 @@ type ActionState =
       code: "SUCCESS"
       message: string;
     }
-    | {
+  | {
       code: "EXISTS_ERROR";
       key: string;
       message: string;
-    }
+  }
   | {
       code: "INTERNAL_ERROR";
       err: any;
     }
   | {
-    code: "VALIDATION_ERROR";
-    fieldErrors: {
-      [field: string]: string[];
-    };
+      code: "VALIDATION_ERROR";
+      fieldErrors: {
+        [field: string]: string[];
+      };
   };
